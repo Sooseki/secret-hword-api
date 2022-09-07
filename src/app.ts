aabea5936c59b7ee18de4c5243ceddf5310df83e
Juan Restrepo Torres <juan.m13@hotmail.fr>
2022-09-07 17:57:57 +0200

    some changes

--- src/app.ts
+++ src/app.ts
@@ -1,54 +1,21 @@
 /* eslint-disable prettier/prettier */
-import cors from 'cors';
-import express, { json } from 'express';
-import { Log } from './classes/Logging/Log';
-import { DefaultErrorHandler } from './middlewares/error-handler';
-import expressWs from 'express-ws';
-import { USER_ROUTES } from './routes/user/UserController';
+import cors from 'cors'
+import express, { json } from 'express'
+import { Log } from './classes/Logging/Log'
+import { DefaultErrorHandler } from './middlewares/error-handler'
+import expressWs from 'express-ws'
+import { USER_ROUTES } from './routes/user/UserController'
 import path from 'path';
 import { Socket } from 'socket.io';
-<<<<<<< HEAD
+import { Server } from 'socket.io'
 
-const app = express();
 /**
  * @type {Socket}
  */
 
 /**
- * On dit à Express que l'on souhaite parser le body des requêtes en JSON
- *
- * @example app.post('/', (req) => req.body.prop)
- */
-app.use(json());
-=======
-import { Server } from 'socket.io'
->>>>>>> 1f94e4af740344ae410abe54974026b3f55f0941
-
-/**
- * On dit à Express que l'on souhaite autoriser tous les noms de domaines
- * à faire des requêtes sur notre API.
- */
-app.use(cors());
-
-/**
- * Toutes les routes CRUD pour les animaux seronts préfixées par `/pets`
- */
-// app.ws('/user', USER_ROUTES)
-
-/**
  * On créé une nouvelle "application" express
  */
-<<<<<<< HEAD
-
-const http = require('http').createServer(app);
-const port = process.env.PORT || 5555;
-const io = require('socket.io')(http , {
-  cors: {
-    origin: "http://localhost3000",
-    methods: ["GET", "POST"],
-  }
-});
-=======
 const app = express();
 const http = require('http');
 const port = process.env.PORT || 5555;
@@ -62,16 +29,12 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
   }
 })
->>>>>>> 1f94e4af740344ae410abe54974026b3f55f0941
 
 app.use(express.static('public'));
 
-app.get('/game', (req, res) => {
+app.get('/game',(req, res) => {
   res.sendFile(path.join(__dirname, 'template/game/morpion.html'));
-});
-app.get('/david', (req, res) => {
-  res.send('salut');
-});
+})
 
 app.get("/david", (req, res) => {
   res.send("salut")
@@ -83,29 +46,15 @@ server.listen(port, () => {
 
 let rooms = [];
 
-io.on('connection', socket => {
+io.on('connection', (socket) => {
   console.log(`[connection] ${socket.id}`);
 
-<<<<<<< HEAD
-  socket.on('disconnect', () => {
-    console.log(`[disconnect] ${socket.id}`);
-    let room = null;
-
-    rooms.forEach(r => {
-      r.players.forEach(p => {
-        if (p.socketId === socket.id && p.host) {
-          room = r;
-          rooms = rooms.filter(r => r !== room);
-        }
-      });
-    });
-=======
   socket.on('playerData', (player) => {
       console.log(`[playerData] ${player.username}`);
 
       let room = null;
 
-      if (!player.roomId) {
+      if (player.roomId === "") {
           room = createRoom(player);
           console.log(`[create room ] - ${room.id} - ${player.username}`);
       } else {
@@ -157,7 +106,6 @@ io.on('connection', socket => {
               }
           })
       })
->>>>>>> 1f94e4af740344ae410abe54974026b3f55f0941
   });
 });
 
@@ -176,8 +124,6 @@ function roomId() {
   return Math.random().toString(36).substr(2, 9);
 }
 /**
-<<<<<<< HEAD
-=======
  * On dit à Express que l'on souhaite parser le body des requêtes en JSON
  *
  * @example app.post('/', (req) => req.body.prop)  
@@ -197,7 +143,6 @@ app.use(cors())
 
 
 /**
->>>>>>> 1f94e4af740344ae410abe54974026b3f55f0941
  * Gestion des erreurs
  */
-app.use(DefaultErrorHandler);
+ app.use(DefaultErrorHandler)
