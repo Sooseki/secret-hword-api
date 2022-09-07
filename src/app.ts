1f94e4af740344ae410abe54974026b3f55f0941
aparent <alexp90300@gmail.com>
2022-09-07 10:42:16 +0200

    creation room

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,6 @@ const io = new Server(server, {
   }
 })
 
-app.use('/jquery', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 app.use(express.static('public'));
 
 app.get('/game',(req, res) => {
@@ -49,8 +48,53 @@ let rooms = [];
 
 io.on('connection', (socket) => {
   console.log(`[connection] ${socket.id}`);
-  
-    socket.on('disconnect', () => {
+
+  socket.on('playerData', (player) => {
+      console.log(`[playerData] ${player.username}`);
+
+      let room = null;
+
+      if (!player.roomId) {
+          room = createRoom(player);
+          console.log(`[create room ] - ${room.id} - ${player.username}`);
+      } else {
+          room = rooms.find(r => r.id === player.roomId);
+
+          if (room === undefined) {
+              return;
+          }
+
+          player.roomId = room.id;
+          room.players.push(player);
+      }
+
+      socket.join(room.id);
+
+      io.to(socket.id).emit('join room', room.id);
+
+      if (room.players.length === 2) {
+          io.to(room.id).emit('start game', room.players);
+      }
+  });
+
+  socket.on('get rooms', () => {
+      io.to(socket.id).emit('list rooms', rooms);
+  });
+
+  socket.on('play', (player) => {
+      console.log(`[play] ${player.username}`);
+      io.to(player.roomId).emit('play', player);
+  });
+
+  socket.on('play again', (roomId) => {
+      const room = rooms.find(r => r.id === roomId);
+
+      if (room && room.players.length === 2) {
+          io.to(room.id).emit('play again', room.players);
+      }
+  })
+
+  socket.on('disconnect', () => {
       console.log(`[disconnect] ${socket.id}`);
       let room = null;
 
@@ -65,6 +109,20 @@ io.on('connection', (socket) => {
   });
 });
 
+function createRoom(player) {
+  const room = { id: roomId(), players: [] };
+
+  player.roomId = room.id;
+
+  room.players.push(player);
+  rooms.push(room);
+
+  return room;
+}
+
+function roomId() {
+  return Math.random().toString(36).substr(2, 9);
+}
 /**
  * On dit à Express que l'on souhaite parser le body des requêtes en JSON
  *
